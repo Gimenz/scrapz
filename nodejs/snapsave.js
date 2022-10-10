fe077ac1b8fad06d9e34c9bf5dd089a91f20ade2
Gimenz <agaaja187@gmail.com>
2022-10-10 18:32:26 +0700

    delete unused var

--- nodejs/snapsave.js
+++ nodejs/snapsave.js
@@ -19,8 +19,6 @@ async function snapsave(url) {
         videoUrl: list[0].querySelector('td > a').href.replace(/\\|"/g, ''),
     }
 }
-// 0    1                         2                                                  3        4         5   6   7     8      9         10        11    
-var _0xc41e = ["", "split", "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/", "slice", "indexOf", "", "", ".", "pow", "reduce", "reverse", "0"];
 
 function x(d, e, f) {
     const g = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/'.split('')
